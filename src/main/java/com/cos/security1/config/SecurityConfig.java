64b9abc2fbffa6d1f4c442cd99660ae1e83abf65
yyh0126 <yyh0126@naver.com>
2023-07-13 21:40:45 +0900

    Feat : Google 회원 프로필 정보 받아오기

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize/postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -33,6 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리 필요
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리 필요. Tip.코드X, 엑세스토큰+사용자정보 O
+        // 1.코드받기(인증) 2.엑세스토큰(권한) 3.사용자프로필 정보 가져오기
+        // 4-1.그 정보를 토대로 회원가입을 자동으로 진행하기도 함
+        // 4-2.추가 회원 정보가 필요하면 추가 회원가입 창을 통해 회원가입 진행
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
     }
 }
